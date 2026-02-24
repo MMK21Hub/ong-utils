1e2438d506df15d870b4fe089d320597a863d64d
MMK21 <50421330+MMK21Hub@users.noreply.github.com>
2026-02-24 11:42:24 +0000

    Allow inputs over the buffer size

--- src/cat.c
+++ src/cat.c
@@ -2,9 +2,20 @@
 #include <unistd.h>
 #define BUFFER_SIZE 512
 
-int main() {
+int main(int argc, char *argv[]) {
     char buffer[BUFFER_SIZE];
-    read(STDIN_FILENO, buffer, BUFFER_SIZE);
-    printf("%s", buffer);
+    int bytes_read = 1;
+    while (1) {
+        bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+        if (bytes_read < 0) {
+            perror("Error reading input stream");
+            return 1;
+        }
+        if (bytes_read > 0) {
+            write(STDOUT_FILENO, buffer, bytes_read);
+        } else {
+            break; // EOF reached
+        }
+    }
     return 0;
 }
