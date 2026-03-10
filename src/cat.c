ee4ad1e9a30004e167cda352af4624f933a290b6
MMK21 <50421330+MMK21Hub@users.noreply.github.com>
2026-03-10 11:15:29 +0000

    Allow reading arbitrary files

--- src/cat.c
+++ src/cat.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+
 #define BUFFER_SIZE 512
 
 int main(int argc, char *argv[]) {
     char buffer[BUFFER_SIZE];
+    
+    FILE *file = stdin;
+    if (argc > 1) {
+        file = fopen(argv[1], "r");
+        if (file == NULL) {
+            perror("Error opening file");
+            return 1;
+        }
+    }
 
     int bytes_read;
     while (1) {
         // Reading the input stream
-        bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+        bytes_read = fread(buffer, sizeof(char), BUFFER_SIZE, file);
         if (bytes_read < 0) {
             perror("Error reading input stream");
             return 1;
@@ -20,5 +31,7 @@ int main(int argc, char *argv[]) {
             break; // EOF reached
         }
     }
+
+    fclose(file);
     return 0;
 }
