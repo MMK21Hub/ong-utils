d7a8e1158651ca9c41c21a6a50b575f551f37a54
MMK21 <50421330+MMK21Hub@users.noreply.github.com>
2026-03-10 11:41:54 +0000

    cat can now cat

--- src/cat.c
+++ src/cat.c
@@ -4,17 +4,8 @@
 
 #define BUFFER_SIZE 512
 
-int main(int argc, char *argv[]) {
+void print_file(FILE *file) {
     char buffer[BUFFER_SIZE];
-    
-    FILE *file = stdin;
-    if (argc > 1) {
-        file = fopen(argv[1], "r");
-        if (file == NULL) {
-            perror("Error opening file");
-            return 1;
-        }
-    }
 
     int bytes_read;
     while (1) {
@@ -22,7 +13,7 @@ int main(int argc, char *argv[]) {
         bytes_read = fread(buffer, sizeof(char), BUFFER_SIZE, file);
         if (bytes_read < 0) {
             perror("Error reading input stream");
-            return 1;
+            exit(1);
         }
         if (bytes_read > 0) {
             // Actually print the output!
@@ -31,7 +22,29 @@ int main(int argc, char *argv[]) {
             break; // EOF reached
         }
     }
+}
 
+void open_and_print_file(char *file_name) {
+    FILE *file = fopen(file_name, "r");
+    if (file == NULL) {
+        // TODO include filename in error msg
+        perror("Error opening file");
+        exit(1);
+    }
+    print_file(file);
     fclose(file);
+}
+
+int main(int argc, char *argv[]) {    
+    if (argc < 2) {
+        // No args provided, so print stdin
+        print_file(stdin);
+        return 0;
+    }
+
+    // Concatenate the provided file paths
+    for (int i = 1; i < argc; i++) {
+        open_and_print_file(argv[i]);
+    }
     return 0;
 }
